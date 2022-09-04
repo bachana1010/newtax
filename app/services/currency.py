7fc125d64bfa1b504d87094fc5655a4fe372643f
bachana10 <b.mindiashvili97@gmail.com>
2022-09-04 14:43:40 +0400

    Initial commit

---
+++ app/services/currency.py
@@ -0,0 +1,35 @@
+import requests
+
+class currency_course:
+    def get_response(self):
+        return requests.get('https://any.ge/currency/api.php?info=yvela&fbclid=IwAR2oIXPWQoDNjVkj9zADQukl0HeA08WehIpN6MsH8OIMtBLFFBGnWukIvVU')
+
+    def get_all_currencies(self):
+        currencies_items = {}
+
+        response = self.get_response()
+        currencies = response.json()['currency']
+
+        for item in currencies:
+            currencies_items[item['cur_code']] = item['cur_value']
+
+        return currencies_items
+
+    def get_specific_currencies(self):
+        currencies = self.get_all_currencies()
+        main_dict = {}
+
+        for currency_code, rate_valley in currencies.items():
+            if currency_code == "USD":
+                main_dict[currency_code] = rate_valley
+            elif currency_code == "GBP":
+                main_dict[currency_code] = rate_valley
+            elif currency_code == "EUR":
+                main_dict[currency_code] = rate_valley
+            elif currency_code == "TRY":
+                main_dict[currency_code] = rate_valley
+            elif currency_code == "CHF":
+                main_dict[currency_code] = rate_valley
+
+        return main_dict
+
